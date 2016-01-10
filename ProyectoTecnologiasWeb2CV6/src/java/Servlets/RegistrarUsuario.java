5cf9e4d67107aceb08766ed73b07c0074a3dd1b8
liobouchan <leo.bouchan@gmail.com>
2016-01-10 00:16:05 -0600

    Extraidos los datos, solo falta escribir archivo

---
+++ ProyectoTecnologiasWeb2CV6/src/java/Servlets/RegistrarUsuario.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jdom2.*;
+import org.jdom2.input.SAXBuilder;
+
+/**
+ *
+ * @author lio
+ */
+public class RegistrarUsuario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RegistrarUsuario</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet RegistrarUsuario at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        try{
+            
+            ServletContext servletContext = request.getServletContext();
+            File archivoXML = new File(servletContext.getRealPath("/XML/Usuarios.xml"));
+            FileInputStream fileInputStream = new FileInputStream(archivoXML);
+            SAXBuilder saxBuilder = new SAXBuilder();
+            Document document = saxBuilder.build(archivoXML);
+            Element nodoRaiz = document.getRootElement();
+            
+            fileInputStream.close();
+            
+            Element Nuevousuario = new Element("Usuario");
+            Element user = new Element("user").setText(usuario);
+            Element pass = new Element("pass").setText(password);
+            
+            nodoRaiz.addContent(user);
+            nodoRaiz.addContent(pass);
+            
+            document.setContent(nodoRaiz);
+            
+        } catch (JDOMException ex) {
+            Logger.getLogger(RegistrarUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
